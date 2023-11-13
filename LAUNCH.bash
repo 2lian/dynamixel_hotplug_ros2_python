afc67a4d0f6abb692e2fa030f85ed68b51ff6907
elian_jetson_moonbot <elian.neppel@wanadoo.fr>
2023-11-13 11:00:58 +0900

    forgot launchbash

---
+++ LAUNCH.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+# run this inside this folder
+cd ${ROS2_DYNA_WS}
+source ${ROS2_INSTALL_PATH}/setup.bash
+colcon build --symlink-install --packages-select motor_controller
+. install/setup.bash
+export RCUTILS_CONSOLE_OUTPUT_FORMAT="{message}"
+export RCUTILS_COLORIZED_OUTPUT=1
+#export ROS_DOMAIN_ID=58
+ros2 launch src/motor_controller/launch/lvl_02_ik.py

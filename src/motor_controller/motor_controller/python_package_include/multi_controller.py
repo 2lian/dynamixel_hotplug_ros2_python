537a862697125fe64d988943b48d484204f5c50d
2lian <70491689+hubble14567@users.noreply.github.com>
2023-11-26 12:01:18 +0900

    reverted prev commit

--- src/motor_controller/motor_controller/python_package_include/multi_controller.py
+++ src/motor_controller/motor_controller/python_package_include/multi_controller.py
@@ -517,7 +517,7 @@ class MotorHandler:
         if not self.motor_list:
             return True
 
-        self.delete_dead_motors()  # because unresponsive motors make the whole read fail
+        # self.delete_dead_motors()  # because unresponsive motors make the whole read fail
         dxl_comm_result = self.groupBulkRead.txRxPacket()
         if dxl_comm_result != COMM_SUCCESS:
             print("Request_update failed")

52e594d6b80bdf59543176e97d416880ae4cbea1
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-08-19 14:24:15 +0900

    python auto version

--- src/motor_controller/setup.py
+++ src/motor_controller/setup.py
@@ -1,33 +1,36 @@
 from setuptools import setup
 from glob import glob
-package_name = 'motor_controller'
+from sys import version_info
+
+package_name = "motor_controller"
+python_version = f"{version_info.major}.{version_info.minor}"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('lib/python3.10/site-packages/python_package_include', glob(f'{package_name}/python_package_include/*.py')),
-        ('lib/python3.10/site-packages/dynamixel_sdk',
-         glob(f'{package_name}/python_package_include/dynamixel_sdk/*')),
-        ('lib/python3.8/site-packages/python_package_include', glob(f'{package_name}/python_package_include/*.py')),
-        ('lib/python3.8/site-packages/dynamixel_sdk',
-         glob(f'{package_name}/python_package_include/dynamixel_sdk/*')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            f"lib/{python_version}/site-packages/python_package_include",
+            glob(f"{package_name}/python_package_include/*.py"),
+        ),
+        (
+            f"lib/{python_version}/site-packages/dynamixel_sdk",
+            glob(f"{package_name}/python_package_include/dynamixel_sdk/*"),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Elian_NEPPEL',
-    maintainer_email='example@dc.tohoku.ac.jp',
-    description='interfaces with the motors',
-    license='Apache License 2.0',
+    maintainer="Elian_NEPPEL",
+    maintainer_email="example@dc.tohoku.ac.jp",
+    description="interfaces with the motors",
+    license="Apache License 2.0",
     entry_points={
-        'console_scripts': [
-            f'u2d2_dyna_controller = {package_name}.u2d2_dyna_controller:main',
-            f'angle_remapper = {package_name}.angle_remapper:main',
-
+        "console_scripts": [
+            f"u2d2_dyna_controller = {package_name}.u2d2_dyna_controller:main",
+            f"angle_remapper = {package_name}.angle_remapper:main",
         ],
     },
 )
